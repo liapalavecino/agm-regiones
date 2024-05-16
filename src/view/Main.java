9f3d4302cefe0724b6a2649188b53ff4954e08db
Lia Palavecino <soledadlia@gmail.com>
2024-05-16 14:07:41 -0300

    Acomodando interfaz 'Main'

--- src/view/Main.java
+++ src/view/Main.java
@@ -84,74 +84,78 @@ public class Main {
 		diseñandoRegiones = new JFrame();
 		diseñandoRegiones.setBackground(new Color(240, 240, 240));
 		diseñandoRegiones.setResizable(false);
-		diseñandoRegiones.setBounds(410, 160, 710, 500);
+		diseñandoRegiones.setBounds(410, 160, 750, 500);
 		diseñandoRegiones.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		diseñandoRegiones.getContentPane().setLayout(null);
 		diseñandoRegiones.setFocusable(true);
 		
 		diseñandoRegiones.setTitle("Diseñando regiones");
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Diseñando Regiones");
+		lblNewLabel_1_1.setFont(new Font("Dialog", Font.BOLD, 14));
+		lblNewLabel_1_1.setBounds(63, 10, 157, 22);
+		diseñandoRegiones.getContentPane().add(lblNewLabel_1_1);
+		
 		Coordinate coordenadaInicial = new Coordinate(-40.5, -64.45);
 		
 		mapa = new JMapViewer();
-		mapa.setBounds(240, 0, 454, 460);
+		mapa.setBounds(278, 0, 454, 460);
 		diseñandoRegiones.getContentPane().add(mapa);
 
-		JLabel lblNewLabel_1_3 = new JLabel("Agregue la similitud");
-		lblNewLabel_1_3.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblNewLabel_1_3.setBounds(-155, 89, 331, 14);
-		mapa.add(lblNewLabel_1_3);
+//		JLabel lblNewLabel_1_3 = new JLabel("Agregue la similitud");
+//		lblNewLabel_1_3.setFont(new Font("Tahoma", Font.BOLD, 11));
+//		lblNewLabel_1_3.setBounds(-155, 89, 331, 14);
+//		mapa.add(lblNewLabel_1_3);
 		JLabel lblNewLabel_1 = new JLabel("Agregue la similaridad");
-		lblNewLabel_1.setBounds(10, 114, 147, 14);
+		lblNewLabel_1.setBounds(20, 142, 147, 14);
 		diseñandoRegiones.getContentPane().add(lblNewLabel_1);
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 11));
-
-		JLabel lblNewLabel_1_1 = new JLabel("Diseñando Regiones");
-		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblNewLabel_1_1.setBounds(10, 11, 152, 14);
-		diseñandoRegiones.getContentPane().add(lblNewLabel_1_1);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 12));
 
 		comboBoxCiudad1 = new JComboBox<>();
-		comboBoxCiudad1.setBounds(10, 75, 100, 22);
+		comboBoxCiudad1.setBounds(20, 75, 100, 22);
 		diseñandoRegiones.getContentPane().add(comboBoxCiudad1);
 
 		comboBoxCiudad2 = new JComboBox<>();
-		comboBoxCiudad2.setBounds(120, 75, 100, 22);
+		comboBoxCiudad2.setBounds(168, 75, 100, 22);
 		diseñandoRegiones.getContentPane().add(comboBoxCiudad2);
 
-		JLabel lblNewLabel_1_2 = new JLabel("coordenada 1");
-		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblNewLabel_1_2.setBounds(10, 54, 100, 14);
+		JLabel lblNewLabel_1_2 = new JLabel("Coordenada 1");
+		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_1_2.setBounds(28, 51, 89, 14);
 		diseñandoRegiones.getContentPane().add(lblNewLabel_1_2);
 
-		JLabel lblNewLabel_1_2_1 = new JLabel("coordenada 2");
-		lblNewLabel_1_2_1.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblNewLabel_1_2_1.setBounds(120, 54, 100, 14);
+		JLabel lblNewLabel_1_2_1 = new JLabel("Coordenada 2");
+		lblNewLabel_1_2_1.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_1_2_1.setBounds(175, 51, 89, 14);
 		diseñandoRegiones.getContentPane().add(lblNewLabel_1_2_1);
 
 		indiceSimilitud = new JTextField();
-		indiceSimilitud.setBounds(10, 140, 30, 20);
+		indiceSimilitud.setBounds(168, 138, 100, 22);
 		diseñandoRegiones.getContentPane().add(indiceSimilitud);
 		indiceSimilitud.setColumns(10);
 
 		btnAGM = new JButton("Mostrar AGM");
-		btnAGM.setBounds(10, 426, 126, 23);
+		btnAGM.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnAGM.setBounds(79, 430, 126, 23);
 		diseñandoRegiones.getContentPane().add(btnAGM);
 
-		JLabel lblCantidadRegiones = new JLabel("Cantidad de regiones:");
-		lblCantidadRegiones.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblCantidadRegiones.setBounds(10, 183, 126, 14);
+		JLabel lblCantidadRegiones = new JLabel("Cantidad de regiones");
+		lblCantidadRegiones.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblCantidadRegiones.setBounds(20, 241, 147, 14);
 		diseñandoRegiones.getContentPane().add(lblCantidadRegiones);
 
 		textField_Regiones = new JTextField();
-		textField_Regiones.setBounds(120, 212, 20, 20);
+		textField_Regiones.setBounds(168, 237, 100, 22);
 		diseñandoRegiones.getContentPane().add(textField_Regiones);
 		textField_Regiones.setColumns(10);
 
 		btnSepararRegiones = new JButton("Aceptar");
-		btnSepararRegiones.setBounds(10, 211, 89, 23);
+		btnSepararRegiones.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnSepararRegiones.setBounds(90, 282, 94, 23);
 		diseñandoRegiones.getContentPane().add(btnSepararRegiones);
 		btnCrearArista = new JButton("Agregar");
-		btnCrearArista.setBounds(63, 139, 94, 23);
+		btnCrearArista.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnCrearArista.setBounds(90, 178, 94, 23);
 		diseñandoRegiones.getContentPane().add(btnCrearArista);
 		
 		btnCrearArista.addActionListener(new ActionListener() {			
